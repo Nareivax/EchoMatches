5d1d7a669afb94cf24202e0d097fa96bac39c789
Nareivax <nareivax@gmail.com>
2019-03-10 13:13:44 -0400

    easier just to use the kivy popup function directly (#3)

--- client/select.py
+++ client/select.py
@@ -6,6 +6,8 @@ import socket
 
 from kivy.app import App
 from kivy.uix.screenmanager import Screen, SlideTransition
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
 
 class Select(Screen):
     '''Class for the server select screen on client app'''
@@ -35,12 +37,21 @@ class Select(Screen):
                 continue
             break
 
-        self.manager.transition = SlideTransition(direction="up")
-        self.manager.current = 'login'
-
         if self.EF_SOCKET is None:
+            popup = Popup(title='Connection Error',
+                content=Label(text='Could Not Connect to Server: ' + HOST + ':' + str(PORT)),
+                size_hint=(None, None), size=(400, 200))
+            popup.open()
             print('Could not connect with Server')
-            sys.exit(1)
+            self.reset_inputs()
+        else:
+            self.manager.transition = SlideTransition(direction="up")
+            self.manager.current = 'login'
 
     def _get_socket(self):
         return self.EF_SOCKET
+
+    def reset_inputs(self):
+        '''Clear the text fields'''
+        self.ids['server_ip'].text = ""
+        self.ids['server_port'].text = ""
