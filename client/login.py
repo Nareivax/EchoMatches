0f6d71f18c713485ea8d6f1b966faead4c860d37
Nareivax <Nareivax@gmail.com>
2019-03-15 15:57:12 -0400

    Added in an InviteDialog popup stub, needs to be updated to take in just invite characters and all ⌐

--- client/login.py
+++ client/login.py
@@ -3,6 +3,7 @@
 
 from kivy.app import App
 from kivy.properties import StringProperty
+from kivy.uix.popup import Popup
 from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
 from echo_common.msg_types import MsgType
 from echo_common.status_types import Status
@@ -15,11 +16,38 @@ class Login(Screen):
 
         login_msg = app.setup_msg(MsgType['USERNAME'], login_text)
         response = app.send_msg(login_msg)
+        return response
+
+    def join_lobby(self, login_text):
+        '''Join an already created lobby'''
+        response = self.user_login(login_text)
 
         if response['content'] == Status['OK']:
+            invite_popup = InviteDialog(self)
+            invite_popup.open()
             self.manager.transition = SlideTransition(direction="left")
             self.manager.current = 'lobby'
+
+    def create_lobby(self, login_text):
+        '''Start a new lobby'''
 
     def reset_login(self):
         '''Clear the text fields'''
         self.ids['login'].text = ""
+
+class InviteDialog(Popup):
+
+    def __init__(self, parent, *args):
+        super(InviteDialog, self).__init__(*args)
+
+    def on_error(self, inst, text):
+        if text:
+            self.lb_error.size_hint_y = 1
+            self.size = (400, 150)
+        else:
+            self.lb_error.size_hint_y = None
+            self.lb_error.height = 0
+            self.size = (400, 120)
+
+    def _cancel(self):
+        self.dismiss()
